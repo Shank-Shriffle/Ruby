778fa8baaf63dcf4381033f1d183979796878e50
ShankShriffle <shashankp@shriffle.com>
2022-07-18 19:53:27 +0530

    Datatypes,Operators, If-Else(incomplete)

---
+++ if_else.rb
@@ -0,0 +1,16 @@
+class If_Else 
+	marks = 32
+	
+	if marks >= 80
+		puts "Good Marks"
+	elsif 79 >= marks >= 60
+		puts "Average Marks"
+	elsif 59 >= marks >=33
+		puts "Low Marks"
+	else
+		puts "Fail"
+	end
+
+end	
+		
+		
