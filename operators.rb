ef431508c0861dfb31f5341f07f585eeb121292a
ShankShriffle <shashankp@shriffle.com>
2022-07-19 19:52:31 +0530

    If_else If_else modifier Unless Case for break next redo while while modifier

--- operators.rb
+++ operators.rb
@@ -153,14 +153,14 @@ puts
 
 # ------Range Operators------
  
-# using .. Operator
-range_op = (7 .. 10).to_a
+# using (..) Operator
+range_op = (7 .. 10).to_a()
  
 # displaying result
 puts "#{range_op}"
  
-# using ... Operator
-range_op1 = (7 ... 10).to_a
+# using (...) Operator
+range_op1 = (7 ... 10).to_a()
  
 # displaying result
 puts "#{range_op1}"
